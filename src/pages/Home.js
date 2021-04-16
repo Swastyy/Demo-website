7a257c40e2720b4908f0514cccf0d4cda4dc4328
Swastyy <gupta.swastika521@gmail.com>
2021-04-16 15:21:46 +0530

    see

---
+++ src/pages/Home.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import Header from '../partials/Header';
+import HeroHome from '../partials/HeroHome';
+import FeaturesBlocks from '../partials/FeaturesBlocks';
+
+function Home() {
+  return (
+    <div className="flex flex-col min-h-screen overflow-hidden">
+
+      {/*  Site header */}
+      <Header />
+
+      {/*  Page content */}
+      <main className="flex-grow">
+
+        {/*  Page sections */}
+        <HeroHome />
+        <FeaturesBlocks />
+
+      </main>
+
+    </div>
+  );
+}
+
+export default Home;
